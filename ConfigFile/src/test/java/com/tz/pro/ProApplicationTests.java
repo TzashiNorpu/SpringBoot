8323784f97c8fe901fcb02824ddaa56e32b0cda7
TzashiNorpu <TzashiNorpu@gmail.com>
2020-08-07 10:42:00 +0800

    AutoConfig

--- ConfigFile/src/test/java/com/tz/pro/ProApplicationTests.java
+++ ConfigFile/src/test/java/com/tz/pro/ProApplicationTests.java
@@ -1,13 +1,25 @@
 package com.tz.pro;
 
+import com.tz.pro.bean.Person;
+import com.tz.pro.service.HelloService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ProApplicationTests {
-
+    @Autowired
+    Person person;
+    @Autowired
+    ApplicationContext applicationContext;
+    @Test
+    void test(){
+        System.out.println(applicationContext.containsBean("helloService02"));
+    }
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
