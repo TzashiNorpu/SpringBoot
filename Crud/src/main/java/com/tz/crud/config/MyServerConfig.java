b42c6ff17916523889e19e8a9d8c697f60197b5d
TzashiNorpu <TzashiNorpu@gmail.com>
2020-08-20 19:08:57 +0800

    tutu

--- Crud/src/main/java/com/tz/crud/config/MyServerConfig.java
+++ Crud/src/main/java/com/tz/crud/config/MyServerConfig.java
@@ -3,13 +3,21 @@ package com.tz.crud.config;
 import com.tz.crud.filter.MyFilter;
 import com.tz.crud.listener.MyListener;
 import com.tz.crud.servlet.MyServlet;
+import org.springframework.boot.autoconfigure.web.embedded.TomcatWebServerFactoryCustomizer;
+import org.springframework.boot.autoconfigure.websocket.servlet.JettyWebSocketServletWebServerCustomizer;
 import org.springframework.boot.autoconfigure.websocket.servlet.TomcatWebSocketServletWebServerCustomizer;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
@@ -18,41 +26,29 @@ public class MyServerConfig {
 
     //注册三大组件
     @Bean
-    public ServletRegistrationBean myServlet(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(),"/myServlet");
+    public ServletRegistrationBean myServlet() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(), "/myServlet");
         registrationBean.setLoadOnStartup(1);
         return registrationBean;
     }
 
     @Bean
-    public FilterRegistrationBean myFilter(){
+    public FilterRegistrationBean myFilter() {
         FilterRegistrationBean registrationBean = new FilterRegistrationBean();
         registrationBean.setFilter(new MyFilter());
-        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        registrationBean.setUrlPatterns(Arrays.asList("/hello", "/myServlet"));
         return registrationBean;
     }
 
     @Bean
-    public ServletListenerRegistrationBean myListener(){
+    public ServletListenerRegistrationBean myListener() {
         ServletListenerRegistrationBean<MyListener> registrationBean = new ServletListenerRegistrationBean<>(new MyListener());
         return registrationBean;
     }
 
 
-   /* //配置嵌入式的Servlet容器
-    @Bean
-    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer(){
-        return new EmbeddedServletContainerCustomizer() {
 
-            //定制嵌入式的Servlet容器相关的规则
-            @Override
-            public void customize(ConfigurableEmbeddedServletContainer container) {
-                container.setPort(8083);
-            }
-        };
-    }*/
-
-    // 嵌入式Tomcat除配置文件之外的实现方式
+/*    // 嵌入式Tomcat除配置文件之外的实现方式
     @Bean
     public TomcatWebSocketServletWebServerCustomizer tomcatWebSocketServletWebServerCustomizer(){
         return new TomcatWebSocketServletWebServerCustomizer(){
@@ -61,6 +57,37 @@ public class MyServerConfig {
                 factory.setPort(8753);
             }
         };
+    }*/
+
+    /*
+        @Bean
+        public JettyWebSocketServletWebServerCustomizer jettyWebSocketServletWebServerCustomizer() {
+            return new JettyWebSocketServletWebServerCustomizer() {
+                @Override
+                public void customize(JettyServletWebServerFactory factory) {
+                    factory.setPort(8872);
+                }
+            };
+        }*/
+    // 不同容器通用配置
+   /* @Bean
+    public WebServerFactoryCustomizer webServerFactoryCustomizer() {
+        return new WebServerFactoryCustomizer() {
+            @Override
+            public void customize(WebServerFactory factory) {
+                ((ConfigurableWebServerFactory) factory).setPort(8895);
+            }
+        };
+    }*/
+
+
+    @Component
+    public class CustomizationBean implements
+            WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+        @Override
+        public void customize(ConfigurableServletWebServerFactory server) {
+            server.setPort(9000);
+        }
     }
 
 }
