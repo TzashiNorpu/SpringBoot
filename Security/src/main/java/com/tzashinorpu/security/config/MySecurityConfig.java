73722dc476dc2e9dc0764d9b5a8e632e2e41f856
TzashiNorpu <yzsnuaa@126.com>
2020-09-11 23:03:45 +0800

    SpringBoot2.3.3WithSpringSecurity

--- Security/src/main/java/com/tzashinorpu/security/config/MySecurityConfig.java
+++ Security/src/main/java/com/tzashinorpu/security/config/MySecurityConfig.java
@@ -1,15 +1,24 @@
 package com.tzashinorpu.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+@Configuration
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
         //定制请求的授权规则
         http.authorizeRequests().antMatchers("/").permitAll()
@@ -17,9 +26,11 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level2/**").hasRole("VIP2")
                 .antMatchers("/level3/**").hasRole("VIP3");
 
+
         //开启自动配置的登陆功能，效果，如果没有登陆，没有权限就会来到登陆页面
         http.formLogin().usernameParameter("user").passwordParameter("pwd")
                 .loginPage("/userlogin");
+//        http.formLogin();
         //1、/login来到登陆页
         //2、重定向到/login?error表示登陆失败
         //3、更多详细规定
@@ -33,22 +44,32 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         //2、注销成功会返回 /login?logout 页面；
 
         //开启记住我功能
-        http.rememberMe().rememberMeParameter("remeber");
+        http.rememberMe()/*.rememberMeParameter("remember")*/;
         //登陆成功以后，将cookie发给浏览器保存，以后访问页面带上这个cookie，只要通过检查就可以免登录
         //点击注销会删除cookie
 
     }
+    private class MyPasswordEncoder implements PasswordEncoder {
+        @Override
+        public String encode(CharSequence charSequence) {
+            return charSequence.toString();
+        }
 
+        @Override
+        public boolean matches(CharSequence charSequence, String s) {
+            return s.equals(charSequence.toString());
+        }
+    }
     //定义认证规则
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         //super.configure(auth);
         auth.inMemoryAuthentication()
-                .withUser("zhangsan").password("123456").roles("VIP1","VIP2")
-                .and()
-                .withUser("lisi").password("123456").roles("VIP2","VIP3")
-                .and()
-                .withUser("wangwu").password("123456").roles("VIP1","VIP3");
-
+                .passwordEncoder(new MyPasswordEncoder()).withUser("zhangsan").password("123456").roles("VIP1", "VIP2")
+                .and().passwordEncoder(new MyPasswordEncoder())
+                .withUser("lisi").password("123456").roles("VIP2", "VIP3")
+                .and().passwordEncoder(new MyPasswordEncoder())
+                .withUser("wangwu").password("123456").roles("VIP1", "VIP3");
     }
+
 }
